c2f8b3b94cfd7f595ab21585340209fd4453d311
SUKU <sukuwc@riseup.net>
2018-02-16 18:40:17 +0100

    SUKU e m

---
+++ Core/Protoboard-1120/Gerber/GerberCNV.bat
@@ -0,0 +1,23 @@
+REM =========== RENAME TOP LAYER FILES ================
+
+rename "*- CADCAM Top Copper.GBR"           "*- Top Copper.GTL"  
+rename "*- CADCAM Top Silk Screen.GBR"      "*- Top Silk.GTO"
+rename "*- CADCAM Top Solder Resist.GBR"    "*- Top Resist.GTS"  
+rename "*- CADCAM Top SMT Paste.GBR"        "*- Top Paste.GTP"  
+
+REM =========== RENAME BOTTOM LAYER FILES =============
+
+rename "*- CADCAM Bottom Copper.GBR"        "*- Bottom Copper.GBL"
+rename "*- CADCAM Bottom Silk Screen.GBR"   "*- Bottom Silk.GBO"
+rename "*- CADCAM Bottom Solder Resist.GBR" "*- Bottom Resist.GBS" 
+rename "*- CADCAM Bottom SMT Paste.GBR"     "*- Bottom Paste.GBP"  
+
+
+REM =========== RENAME DRILL & MECH FILES =============
+
+rename "*- CADCAM Mechanical 1.GBR"         "*- Mechanical.GBR"  
+rename "*- CADCAM Mechanical 2.GBR"         "*- Routing.GBR"  
+rename "*- CADCAM Mechanical 3.GBR"         "*- Board Outline.GKO" 
+rename "*- CADCAM Mechanical 4.GBR"         "*- Documentation.GBR"  
+rename "*- CADCAM Drill.DRL"                "*- Drill.DRD"  
+
